4edc89a5aaf71ca6e0be1e0debb9e149983f2010
Shalini Agrawal <shalini.agarwal@harness.io>
2022-01-27 17:08:16 +0530

    clean

--- userService/src/main/java/UserService.java
+++
@@ -1,8 +0,0 @@
-import Controllers.UserController;
-
-public class UserService {
-    public static void main(String[] args) {
-        UserController userController = new UserController();
-        userController.apis();
-    }
-}
