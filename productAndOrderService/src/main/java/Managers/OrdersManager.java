f020b97f2a8ba1aac1483fb878838bcaa0dc7634
Shalini Agrawal <shalini.agarwal@harness.io>
2022-01-27 17:55:17 +0530

    updated

--- productAndOrderService/src/main/java/Managers/OrdersManager.java
+++ productAndOrderService/src/main/java/Managers/OrdersManager.java
@@ -3,6 +3,8 @@ package Managers;
 import Entities.Order;
 
 import Handlers.MorphiaHandler;
+import com.example.dummy.UserExistClient;
+import com.example.dummy.UserExistServer;
 
 import java.util.List;
 
@@ -10,18 +12,32 @@ public class OrdersManager {
 
     MorphiaHandler morphiaHandler;
     ProductsManager productsManager;
+    UserExistClient userExistClient;
+    UserExistServer userExistServer;
 
     public OrdersManager() {
         morphiaHandler = new MorphiaHandler();
         productsManager = new ProductsManager();
+        userExistClient = new UserExistClient();
+        userExistServer = new UserExistServer();
     }
 
     public String insert(Order order) {
         if (order.getProducts().length == 0)
             return "No Products added";
-//        List<Object> lis = morphiaHandler.getObjById(order.getUserId(), User.class);
-//        if (lis.size() == 0)
-//            return "user doesn't exist";
+        // Starting gRPC Server
+        new Thread(new Runnable() {
+            public void run() {
+                try {
+                    userExistServer.server();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+
+        if (!userExistClient.doesUserExist(order.getUserId()))
+            return "user doesn't exist";
         for (int x : order.getProducts()) {
             List<Object> l = productsManager.getProductById(x);
             if (l.size() == 0)
